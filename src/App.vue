072c0122c36acd9396a1069cda3c5a99c0c04369
DoubledConG <992196871@qq.com>
2023-11-09 23:18:16 +0800

    feat: add the input and output

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <script setup lang="ts">
+import Logo from "@/components/Logo.vue";
+import { ref, computed } from "vue";
+
+const output = computed(() => (binary.value ? binary.value : 0));
+const binary = ref();
+
+const bin2dec = () => {
+	console.log(111);
+
+	// output.value = binary.value
+};
 </script>
 
 <template>
-  <h1>Bin2Dec</h1>
+	<Logo />
+	<div class="decimal">Output: {{ output }}</div>
+	<input
+		class="binary"
+		v-model="binary"
+		placeholder="Please enter 0 and 1 here..."
+		@change="bin2dec"
+	/>
 </template>
 
 <style scoped>
+.binary {
+	font-size: 24px;
+	padding: 10px 30px;
+}
+.decimal {
+	font-size: 24px;
+	margin-bottom: 1em;
+}
 </style>
