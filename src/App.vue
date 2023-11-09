7b6041f3efea5c09d764f205ac26e3d4fbada87d
DoubledConG <992196871@qq.com>
2023-11-09 23:33:33 +0800

    style: add google fonts

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,14 @@ const bin2dec = () => {
 		placeholder="Please enter 0 and 1 here..."
 		@change="bin2dec"
 	/>
+
+	<p class="user-stories">
+		<em>User Stories</em><br />
+		1. User can enter up to 8 binary digits in one input field<br />
+		2. User must be notified if anything other than a 0 or 1 was entered<br />
+		3. User views the results in a single output field containing the
+		decimal (base 10) equivalent of the binary number that was entered
+	</p>
 </template>
 
 <style scoped>
@@ -31,5 +39,14 @@ const bin2dec = () => {
 .decimal {
 	font-size: 24px;
 	margin-bottom: 1em;
+}
+.user-stories {
+	margin-top: 5em;
+	background-color: #3e3e3e;
+	padding: 10px 20px;
+	border-radius: 10px;
+	font-size: 16px;
+	line-height: 2;
+	text-align: left;
 }
 </style>
